ca56f6ce685ef5376203143f9692c9a40b2e75cc
Arxa <manabis2@yahoo.gr>
2017-03-26 18:44:38 +0300

    Project Completed

---
+++ cmake-build-debug/CMakeFiles/Fractal_Images_Generator.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fractal_Images_Generator.dir/main.cpp.obj"
+  "CMakeFiles/Fractal_Images_Generator.dir/Bitmap.cpp.obj"
+  "CMakeFiles/Fractal_Images_Generator.dir/Mandelbrot.cpp.obj"
+  "CMakeFiles/Fractal_Images_Generator.dir/ZoomList.cpp.obj"
+  "CMakeFiles/Fractal_Images_Generator.dir/Zoom.cpp.obj"
+  "CMakeFiles/Fractal_Images_Generator.dir/FractalCreator.cpp.obj"
+  "CMakeFiles/Fractal_Images_Generator.dir/RGB.cpp.obj"
+  "Fractal_Images_Generator.pdb"
+  "Fractal_Images_Generator.exe"
+  "Fractal_Images_Generator.exe.manifest"
+  "libFractal_Images_Generator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fractal_Images_Generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
